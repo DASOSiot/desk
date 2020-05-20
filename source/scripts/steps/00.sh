307ff99c41f40d694e0beee0bf6afb6a2720c1f2
yei <joaquin.estay@dasos.io>
2020-05-19 23:23:07 -0400

    S00: Swap File

--- source/scripts/steps/00.sh
+++ source/scripts/steps/00.sh
@@ -4,6 +4,13 @@
 echo ""
 echo "STEP 00: Defaults"
 # STEP: 00. START.
+# Swap File
+fallocate -l 4G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+# OPT Structure
 mkdir /opt/vendor
 mkdir /opt/vendor/dasos
 cd /opt/vendor/dasos
