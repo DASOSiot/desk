7e4d11be9465efb79b07dc6f1ba3fc997059c7f6
yei <joaquin.estay@dasos.io>
2020-05-19 23:38:03 -0400

    Seahorse

--- source/scripts/steps/05.sh
+++ source/scripts/steps/05.sh
@@ -3,7 +3,7 @@
 echo ""
 echo " STEP 05: Display Manager."
 # STEP 05: START.
-sudo apt-get install -y lightdm accountsservice seahorse
+sudo apt-get install -y lightdm accountsservice
 # STEP 05: END.
 echo ""
 echo "STEP 05: done!"
