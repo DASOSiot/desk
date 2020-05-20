8cd41d745c4e7ed618498e8bb113e27a163e493d
yei <joaquin.estay@dasos.io>
2020-05-20 16:07:33 -0400

    vendors

--- scripts/steps/11.sh
+++ scripts/steps/11.sh
@@ -7,6 +7,7 @@ apt install -y lxde-core lxde-common # Desktop: Core & Tools
 apt install -y openbox obconf # Desktop: Window Manager
 apt install -y alacarte # Desktop: Menu Manager
 apt install -y seahorse #Desktop: User Tools
+apt install -y xfce4-notifyd xfce4-power-manager xfce4-power-manager-plugins #XFCE Addons
 apt install -y network-manager network-manager-gnome network-manager-openvpn-gnome # Desktop: Network Manager
 apt install -y alsa-utils pavucontrol pulseaudio # Desktop: Audio Manager
 apt install -y xawtv tv-fonts # Desktop: TV Viewer
