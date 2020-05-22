dfdfd408898f871b09b22ef12cb8b926a8a95249
yei <joaquin.estay@dasos.io>
2020-05-22 14:05:05 -0400

    0.0.1

--- install.sh
+++ install.sh
@@ -15,12 +15,12 @@ bash ./scripts/steps/00.sh && # Defaults
 bash ./scripts/steps/01.sh && # Update & Upgrade
 bash ./scripts/steps/02.sh && # Display Options
 bash ./scripts/steps/03.sh && # Graphic Engine
-bash ./scripts/steps/04.sh && # Graphic Engine: Turbo (compiled)
+#bash ./scripts/steps/04.sh && # Graphic Engine: Turbo (compiled)
 bash ./scripts/steps/05.sh && # Display Manager
 bash ./scripts/steps/06.sh && # Display Manager: Autologin
 bash ./scripts/steps/07.sh && # Network Devices
 bash ./scripts/steps/08.sh && # Common Tools
-bash ./scripts/steps/09.sh && # Bluetooth: Upgrade (compiled)
+#bash ./scripts/steps/09.sh && # Bluetooth: Upgrade (compiled)
 bash ./scripts/steps/10.sh && # Users & Groups
 bash ./scripts/steps/11.sh && # XX
 bash ./scripts/steps/12.sh && # YY
